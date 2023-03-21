f9a3bad231feffd8ff1c64c9fc89e145aa36509a
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-03-21 14:48:26 +0530

    variables

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-console.log('hey javaScript');
-console.log("Abhishek Dubalgunde here");
+/*Intro. 1*/
+// console.log('hey javaScript');
+// console.log("Abhishek Dubalgunde here");
 
-window.alert("Are you sure?...");
+// window.alert("Are you sure?...");
+
+/*Variables 2 */
+let num;
+num=20;
+let sname='Abhishek D'    //you can take ("")as well. and semicolon is optional
+let check=true;
+
+console.log(num)
+console.log(check)
+console.log(sname)
+
+
+document.getElementById("p1").innerHTML=sname+' is my name.';
+document.getElementById("p3").innerHTML=check+" okay then number is "+num
+document.getElementById("p2").innerHTML="And number is "+num;
