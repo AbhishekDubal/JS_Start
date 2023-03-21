0bcbc3de8432bcf43cfba72d7a398efdd400be09
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-03-21 18:48:11 +0530

    user input

--- index.js
+++ index.js
@@ -20,22 +20,39 @@
 // document.getElementById("p2").innerHTML="And number is "+num;
 
 /*Operator3 */
-let number=10;
+// let number=10;
 
-let number1=number+2;
-let number2=number-1;
-let number3=number*2;
-let number4=number/2;
-let number5=number%2;
+// let number1=number+2;
+// let number2=number-1;
+// let number3=number*2;
+// let number4=number/2;
+// let number5=number%2;
 
-console.log(number);
-console.log(number1);
-console.log(number3+" is number 3")
-console.log('number 4 result is: '+number4);
-console.log(number5)
+// console.log(number);
+// console.log(number1);
+// console.log(number3+" is number 3")
+// console.log('number 4 result is: '+number4);
+// console.log(number5)
 
-let marks=32
-marks+=10;
-console.log(marks)
+// let marks=32
+// marks+=10;
+// console.log(marks)
 
-document.getElementById("p3").innerHTML=number4
+// document.getElementById("p3").innerHTML=number4
+
+/*4.Taking input from User by TWO-ways*/
+//1.Simple way
+/*
+let username=window.prompt('What is your name:');
+console.log(username)
+*/
+
+//2.Another way(little advance)
+/*
+let job;
+document.getElementById('button_id').onclick=function(){
+    job=document.getElementById('text_id').value
+    console.log(job);
+    // document.getElementById('label_id').innerHTML="Enter the here Designation:"
+}
+*/
