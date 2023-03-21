ab2b83e2834c421244e1db4d110cd3a2bb5cc124
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-03-21 17:19:49 +0530

    Operator

--- index.js
+++ index.js
@@ -5,16 +5,37 @@
 // window.alert("Are you sure?...");
 
 /*Variables 2 */
-let num;
-num=20;
-let sname='Abhishek D'    //you can take ("")as well. and semicolon is optional
-let check=true;
+// let num;
+// num=20;
+// let sname='Abhishek D'    //you can take ("")as well. and semicolon is optional
+// let check=true;
 
-console.log(num)
-console.log(check)
-console.log(sname)
+// console.log(num)    //number
+// console.log(check)  //boolean(true/false)
+// console.log(sname)  //String
 
 
-document.getElementById("p1").innerHTML=sname+' is my name.';
-document.getElementById("p3").innerHTML=check+" okay then number is "+num
-document.getElementById("p2").innerHTML="And number is "+num;
+// document.getElementById("p1").innerHTML=sname+' is my name.';
+// document.getElementById("p3").innerHTML=check+" okay then number is "+num
+// document.getElementById("p2").innerHTML="And number is "+num;
+
+/*Operator3 */
+let number=10;
+
+let number1=number+2;
+let number2=number-1;
+let number3=number*2;
+let number4=number/2;
+let number5=number%2;
+
+console.log(number);
+console.log(number1);
+console.log(number3+" is number 3")
+console.log('number 4 result is: '+number4);
+console.log(number5)
+
+let marks=32
+marks+=10;
+console.log(marks)
+
+document.getElementById("p3").innerHTML=number4
