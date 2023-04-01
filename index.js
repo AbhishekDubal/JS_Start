52f93662471f69ea755499d6df24ee35a7dcf2dc
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-04-02 02:34:09 +0530

    Anonymous Object

--- index.js
+++ index.js
@@ -1566,8 +1566,8 @@ displayAllDetails(car2);
 displayAllDetails(car3);
 */
 
-///\(59). Array of object references :-
-
+///\(59). Array of object references :-\\\\\\\\\\\\\\\\\\\\\\
+/*
 class Car 
 {
     constructor(model,year,color){
@@ -1597,7 +1597,7 @@ cars.forEach(car => {
 cars.forEach(car => {
     console.log(`Model: ${car.model}\nYear: ${car.year}\nColor: ${car.color}\n`);
   });
-  /*output:-
+//*\\\\\output:-
 Model: Tata
 Year: 2022
 Color: White
@@ -1608,5 +1608,48 @@ Color: Dark_blue
 
 Model: Ferrari
 Year: 2015
-Color: Sky_blue*/
+Color: Sky_blue
+*/
 
+////(60).Anonymous Objects:
+//Object without reference_variable. It'll make less complicate and we don't have to have lot of names.
+
+class Car 
+{
+    constructor(model,year,color){
+        this.color=color;
+        this.model=model;
+        this.year=year;
+    }
+}
+
+const cars=[new Car("Tata",2022,"White"),new Car("Brezza",2020,'Dark_blue'),new Car("Ferrari",2015,"Sky_blue")];
+
+cars.forEach(element => {
+    console.log(`NAME is: ${element.model}, Year:-${element.year}, color is: ${element.color}`);
+});
+//It's works same as previous topic.
+
+// //you can also add another object like:-
+// class Car {
+//     constructor(model, year, color) {
+//       this.color = color;
+//       this.model = model;
+//       this.year = year;
+//     }
+//   }
+  
+//   const car1 = new Car("Tata", 2022, "White");
+//   const car2 = new Car("Brezza", 2020, "Dark_blue");
+//   const car3 = new Car("Ferrari", 2015, "Sky_blue");
+  
+//   const cars = [car1, car2, car3, new Car("BMW", 2023, "Black")];
+  
+//   console.log(cars[0].color); // Output: White
+//   console.log(cars[2].year); // Output: 2015
+//   console.log(cars[1].model); // Output: Brezza
+  
+//   cars.forEach((car) => {
+//     console.log(car);
+//   });
+  
